f9cfe5d79aecf76fa625ca11235b2b8eb29a2224
Filopatir Emad <filopatir.bakhoum06@eng-st.cu.edu.eg>
2025-12-06 12:12:36 +0200

    Add video processing and player tracking modules

---
+++ src/video/__init__.py
@@ -0,0 +1,8 @@
+"""
+Video Analysis Module for Elite Set-Piece Analytics
+"""
+
+from src.video.video_processor import VideoProcessor
+from src.video.player_tracker import PlayerTracker
+
+__all__ = ['VideoProcessor', 'PlayerTracker']
